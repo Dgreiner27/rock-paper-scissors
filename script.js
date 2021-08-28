be2eb198a6a5cf9458c8fa86120d700113ac464a
Daniel <danielt.greiner@gmail.com>
2021-08-27 18:39:59 -0700

    Added the game function to play 5 rounds

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-function computerPlay() {
-    let random=Math.random();
-    console.log(random);
-    let compAnswer = ["rock", "paper", "scissors"];
+//These are all of the utility functions to 
+//return the correct winning or losing message
 
-    if(random <=0.33){
-        return compAnswer[0];
-    } else if(random <=0.66){
-        return compAnswer[1];
-    } else return compAnswer[2];
-}
-
-function playerWins(){
-    let response = "You win!";
+function playerWins(player, computer){
+    let response = `Woohoo, ${player} beats ${computer}! You win!`;
     console.log(response);
-    return response;
+    return true;
 }
-
-function playerLoses(){
-    let response = "Aw shucks, the computer won!";
+function playerLoses(player, computer){
+    let response = `Aw shucks, ${computer} beats ${player}, the computer won!`;
     console.log(response);
-    return response;
+    return false;
 }
 function tieGame(){
     let response = "It's a draw!";
@@ -31,26 +21,66 @@ function incorrectInput(){
     console.log(response);
     return response;
 }
+function cancelled(){
+    let response="Goodbye!";
+    console.log(response);
+    return response;
+}
+//Randomizer function and single round function
 
-function singleGame(playerSelection, computerSelection){
+function computerPlay() {
+    let random=Math.random();
+    let compAnswer = ["rock", "paper", "scissors"];
+
+    if(random <=0.33){
+        return compAnswer[0];
+    } else if(random <=0.66){
+        return compAnswer[1];
+    } else return compAnswer[2];
+}
+function playRound(playerSelection, computerSelection){
     playerSelection=prompt("Let\'s play rock, paper scissors!");
     computerSelection=computerPlay();
-    console.log(playerSelection);
-    if(playerSelection.toLowerCase()===computerSelection){
+    
+    if(playerSelection===null){
+        return cancelled();
+    } else if(playerSelection.toLowerCase()===computerSelection){
         return tieGame();
     } else if (playerSelection.toLowerCase()==="rock"){
         if(computerSelection==="paper"){
-            return playerLoses();
-        } else return playerWins();
+            return playerLoses(playerSelection, computerSelection);
+        } else return playerWins(playerSelection, computerSelection);
     } else if(playerSelection.toLowerCase()==="paper"){
         if(computerSelection==="rock"){
-            return playerWins();
-        } else return playerLoses();
+            return playerWins(playerSelection, computerSelection);
+        } else return playerLoses(playerSelection, computerSelection);
     } else if(playerSelection.toLowerCase()==="scissors"){
         if(computerSelection==="rock"){
-            return playerLoses();
-        } else return playerWins();
+            return playerLoses(playerSelection, computerSelection);
+        } else return playerWins(playerSelection, computerSelection);
     } else return incorrectInput();
 }
 
-singleGame();
+
+//Only gives player points
+//Need to troubleshoot
+//Need to change end of game to also declare a winner!
+
+
+function game(){
+    let computerScore=0;
+    let playerScore=0;
+
+    for(let i=0; i<5; i++){
+        
+        playRound();
+        if (true){
+            playerScore++;
+        } else if(false){
+            computerScore++;
+        } else console.log('I need to fix this code');
+    }
+    console.log(`Computer has ${computerScore} points. Player has ${playerScore} points.`);
+}
+
+game();
